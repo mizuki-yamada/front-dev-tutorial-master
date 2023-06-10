47568a0fcd61840947eae18989d168768be8c204
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-06-10 22:37:59 +0900

    reduceを使ってDOMを操作

---
+++ 04_JavaScriptの基礎を固める/110_DOM操作練習_文字分割/start/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const el = document.querySelector(".animate-title");
+    const str = el.innerHTML.trim().split("");
+    el.innerHTML = str.reduce((accu, curr) => {
+      curr = curr.replace("", "&nbsp");
+      return `${accu}<span class="char">${curr}</span>`;
+    }, "");
+    
+//     let concatStr = "";
+
+//   for (c of str) {
+//     c = c.replace(" ", "&nbsp");
+//     concatStr += `<span class="char">${c}</span>`;
+//   }
+//   el.innerHTML = concatStr;
+//   console.log(concatStr);
+});
