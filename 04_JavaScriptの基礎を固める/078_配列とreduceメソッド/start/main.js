5a8d36f51224e8286ebec155a2db6d12790a1d99
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-06-10 19:28:14 +0900

    reduceメソッドを実装

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,19 @@
-const arry = [1,2,3,4,5];
+const array = [1, 2, 3, 4, 5];
+
+array.reduce(function (accu, curr) {
+    console.log(accu, curr);
+    //curr:現在何をみているか、各ループで返される値
+    //accu:1つ前のループで見ていたcurr
+     return accu, curr;
+});
+
+const str = "animation";
+const strArry = str.split("");
+
+// console.log(strArry);
+
+const result = strArry.reduce((accu, curr) => {
+  return `${accu}<${curr}>`;
+},"");
+
+console.log(result);
--- 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
+++ 04_JavaScriptの基礎を固める/079_reduce関数を作ってみよう/start/main.js
@@ -1,9 +1,21 @@
-const str = 'animation';
-const strArry = str.split('');
+const str = "animation";
+const strArry = str.split("");
 
 function tag(accu, curr) {
-    return `${accu}<${curr}>`;
+  console.log(accu, curr);
+  return `${accu}<${curr}>`;
 }
 
-const result = strArry.reduce(tag, "");
+function reduce(arry, callback, defaultValue) {
+  //配列の各要素に対して、縮小関数を呼び出す
+  let accu = defaultValue;
+  for (let i = 0; i < arry.length; i++) {
+    let curr = arry[i];
+    accu = callback(accu, curr);
+  }
+
+  return accu;
+}
+
+const result = reduce(strArry, tag, "");
 console.log(result);
