81d267c3f4a1495fff5784b05b56fae1ce6bf0ab
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-06-11 16:37:51 +0900

    JavaScriptを記述

--- 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -1,19 +1,30 @@
-class MobileMenu{
-    constructor(){
-    this.DOM={};
-    this.DOM.btn=document.querySelector('.mobile-menu__btn');
-    this.DOM.container=document.querySelector('#global-container');
-    this.eventType=this._getEventType();
-    //this._addEvent();
-  this.DOM.btn.addEventListener('click',this._toggle.bind(this));
+class MobileMenu {
+  constructor() {
+    this.DOM = {};
+    this.DOM.btn = document.querySelector(".mobile-menu_btn");
+    this.DOM.container = document.querySelector("#global-container");
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof DocumentTouch);
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.container.classList.toggle("menu-open");
+  }
+
+  _addEvent() {
+    this.DOM.btn.addEventListener("click", () => {
+      this._toggle();
+      //bindの代わりにアロー関数でも動作することを確認
+    });
+  }
 }
- 
- 
-_toggle(){
-    this.DOM.container.classList.toggle('menu-open');
-    }
- // _addEvent(){
-     //   this.DOM.btn.addEventListener('click',this._toggle.bind(this))
-   //}
-}
+
 new MobileMenu();
